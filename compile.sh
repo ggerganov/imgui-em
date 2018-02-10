d05b0e71029c6954890e8dece972d6df45b16a8b
Georgi Gerganov <ggerganov@gmail.com>
2018-02-10 18:42:32 +0200

    Import working copy to github

---
+++ compile.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "static const char * BUILD_TIMESTAMP=\"`date`\";" > build_timestamp.h
+
+em++ -std=c++11 -s USE_SDL=2 \
+    ./main.cpp ./imgui_impl_sdl_em.cpp ./external/imgui/imgui.cpp ./external/imgui/imgui_draw.cpp \
+    ./external/imgui/imgui_demo.cpp \
+    -o imgui.js \
+    -I ./external/imgui/ \
+    -I ./external/imgui/examples/libs/gl3w/
