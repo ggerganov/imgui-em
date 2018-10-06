b1c4f649261ea253ed9d88b660140244a7c5987d
Georgi Gerganov <ggerganov@gmail.com>
2018-10-06 17:04:08 +0300

    Update to latest ImGui v1.66 WIP

--- compile.sh
+++ compile.sh
@@ -4,7 +4,7 @@ echo "static const char * BUILD_TIMESTAMP=\"`date`\";" > build_timestamp.h
 
 em++ -std=c++11 -s USE_SDL=2 \
     ./main.cpp ./imgui_impl_sdl_em.cpp ./external/imgui/imgui.cpp ./external/imgui/imgui_draw.cpp \
-    ./external/imgui/imgui_demo.cpp \
+    ./external/imgui/imgui_widgets.cpp ./external/imgui/imgui_demo.cpp \
     -o imgui.js \
     -I ./external/imgui/ \
     -s EXPORTED_FUNCTIONS='["_setArticleWidth", "_main"]' \
--- main.cpp
+++ main.cpp
@@ -90,7 +90,15 @@ void update() {
     // 3. Show the ImGui demo window. Most of the sample code is in ImGui::ShowDemoWindow(). Read its code to learn more about Dear ImGui!
     if (show_demo_window)
     {
-        ImGui::SetNextWindowPos(ImVec2(20, 50), ImGuiCond_FirstUseEver); // Normally user code doesn't need/want to call this because positions are saved in .ini file anyway. Here we just want to make the demo initial state a bit more friendly!
+        // small hack to overwrite the hardcoded position of the demo window
+        static bool firstTime = true;
+        if (firstTime) {
+            ImGui::SetNextWindowPos(ImVec2(20, 50), ImGuiCond_FirstUseEver);
+            ImGui::SetNextWindowSize(ImVec2(550, 680), ImGuiCond_FirstUseEver);
+            ImGui::Begin("ImGui Demo", &show_demo_window, ImGuiWindowFlags_MenuBar);
+            ImGui::End();
+        }
+
         ImGui::ShowDemoWindow(&show_demo_window);
     }
 
